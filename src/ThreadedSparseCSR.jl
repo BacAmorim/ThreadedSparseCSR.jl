7a509477d093b6a58bea00bf6fee893e2bab09a2
Bruno Amorim <amorim.bac@gmail.com>
2021-11-05 18:55:42 +0000

    trying to use @eval

--- src/ThreadedSparseCSR.jl
+++ src/ThreadedSparseCSR.jl
@@ -1,14 +1,49 @@
 module ThreadedSparseCSR
 
 using SparseMatricesCSR
-using FLoops
 using Polyester
+using LinearAlgebra
 
-import SparseMatricesCSR: nzrange
+export csr_bmul!
+#export csr_bmul
 
-#export get_num_threads, set_num_threads
-export serial_csr_mv, threaded_csr_mv, floops_csr_mv, batch_csr_mv
+#include("matmul.jl")
+#include("multithread_mul.jl")
 
-include("matmul.jl")
+using SparseMatricesCSR: nzrange
+
+function csr_bmul!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVector, alpha::Number, beta::Number)
+    
+    A.n == size(x, 1) || throw(DimensionMismatch())
+    A.m == size(y, 1) || throw(DimensionMismatch())
+
+    o = getoffset(A)
+    
+    @batch for row in 1:size(y, 1)
+        @inbounds begin
+            accu = zero(eltype(y))
+            for nz in nzrange(A, row)
+                col = A.colval[nz] + o
+                accu += A.nzval[nz]*x[col]
+            end
+            y[row] = alpha*accu + beta*y[row]
+    
+        end
+    end
+
+    return y
+
+end
+
+import LinearAlgebra: mul!
+
+@eval begin
+
+    function  mul!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVector, alpha::Number, beta::Number)
+        return csr_bmul!(y, A, x, alpha, beta)
+    end
+
+    
+end
 
 end
