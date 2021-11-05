56d9b9c5d286fea814173d823ec5bca0811c266e
Bruno Amorim <amorim.bac@gmail.com>
2021-11-05 18:56:10 +0000

    trying to use @eval

--- src/matmul.jl
+++ src/matmul.jl
@@ -1,53 +1,14 @@
-function serial_csr_mv!(y::Vector, A::SparseMatrixCSR, x::Vector, alpha::Number, beta::Number)
+# Multithreaded multiplication using Polyester.jl @batch
+using SparseMatricesCSR: nzrange
+
+function csr_bmul!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVector, alpha::Number, beta::Number)
     
     A.n == size(x, 1) || throw(DimensionMismatch())
     A.m == size(y, 1) || throw(DimensionMismatch())
 
     o = getoffset(A)
     
-    @inbounds for row in 1:size(y, 1)
-
-        accu = zero(eltype(y))
-        for nz in nzrange(A, row)
-            col = A.colval[nz] + o
-            accu += A.nzval[nz]*x[col]
-        end
-
-        y[row] = alpha*accu + beta*y[row]
-    
-    end
-
-    return y
-
-end
-
-function serial_csr_mv!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVector)
-    
-    serial_csr_mv!(y, A, x, true, false)
-
-end
-
-function serial_csr_mv(A::SparseMatrixCSR, x::AbstractVector)
-
-    T = promote_type(eltype(A), eltype(x))
-    m = A.m
-    y = Vector{T}(undef, m)
-
-    serial_csr_mv!(y, A, x, true, false)
-
-end
-
-
-
-
-function threaded_csr_mv!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVector, alpha::Number, beta::Number)
-    
-    A.n == size(x, 1) || throw(DimensionMismatch())
-    A.m == size(y, 1) || throw(DimensionMismatch())
-
-    o = getoffset(A)
-    
-    @Threads.threads for row in 1:size(y, 1)
+    @batch for row in 1:size(y, 1)
         @inbounds begin
             accu = zero(eltype(y))
             for nz in nzrange(A, row)
@@ -64,100 +25,18 @@ function threaded_csr_mv!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVecto
 end
 
 
-function threaded_csr_mv!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVector)
+function csr_bmul!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVector)
     
-    threaded_csr_mv!(y, A, x, true, false)
+    csr_bmul!(y, A, x, true, false)
 
 end
 
-function threaded_csr_mv(A::SparseMatrixCSR, x::AbstractVector)
+function csr_bmul(A::SparseMatrixCSR, x::AbstractVector)
 
     T = promote_type(eltype(A), eltype(x))
     m = A.m
     y = Vector{T}(undef, m)
 
-    threaded_csr_mv!(y, A, x, true, false)
-
-end
-
-
-
-function floops_csr_mv!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVector, alpha::Number, beta::Number; num_threads = Threads.nthreads())
-    
-    A.n == size(x, 1) || throw(DimensionMismatch())
-    A.m == size(y, 1) || throw(DimensionMismatch())
-
-    o = getoffset(A)
-    
-    @floop ThreadedEx(basesize = size(y, 1) ÷ num_threads) for row in 1:size(y, 1)
-        @inbounds begin
-            accu = zero(eltype(y))
-            for nz in nzrange(A, row)
-                col = A.colval[nz] + o
-                accu += A.nzval[nz]*x[col]
-            end
-            y[row] = alpha*accu + beta*y[row]
-    
-        end
-    end
-
-    return y
-
-end
-
-
-function floops_csr_mv!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVector; num_threads = Threads.nthreads())
-    
-    floops_csr_mv!(y, A, x, true, false; num_threads = num_threads)
-
-end
-
-function floops_csr_mv(A::SparseMatrixCSR, x::AbstractVector; num_threads = Threads.nthreads())
-
-    T = promote_type(eltype(A), eltype(x))
-    m = A.m
-    y = Vector{T}(undef, m)
-
-    floops_csr_mv!(y, A, x, true, false; num_threads = num_threads)
-
-end
-
-function batch_csr_mv!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVector, alpha::Number, beta::Number; num_threads = Threads.nthreads())
-    
-    A.n == size(x, 1) || throw(DimensionMismatch())
-    A.m == size(y, 1) || throw(DimensionMismatch())
-
-    o = getoffset(A)
-    
-    @batch minbatch = size(y, 1) ÷ num_threads for row in 1:size(y, 1)
-        @inbounds begin
-            accu = zero(eltype(y))
-            for nz in nzrange(A, row)
-                col = A.colval[nz] + o
-                accu += A.nzval[nz]*x[col]
-            end
-            y[row] = alpha*accu + beta*y[row]
-    
-        end
-    end
-
-    return y
-
-end
-
-
-function batch_csr_mv!(y::AbstractArray, A::SparseMatrixCSR, x::AbstractVector; num_threads = Threads.nthreads())
-    
-    batch_csr_mv!(y, A, x, true, false; num_threads = num_threads)
-
-end
-
-function batch_csr_mv(A::SparseMatrixCSR, x::AbstractVector; num_threads = Threads.nthreads())
-
-    T = promote_type(eltype(A), eltype(x))
-    m = A.m
-    y = Vector{T}(undef, m)
-
-    batch_csr_mv!(y, A, x, true, false; num_threads = num_threads)
+    csr_bmul!(y, A, x, true, false)
 
 end
