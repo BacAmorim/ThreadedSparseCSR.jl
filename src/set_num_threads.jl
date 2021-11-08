123e35b04dc584a1a561f7a9c710ed5e47d99480
Bruno Amorim <amorim.bac@gmail.com>
2021-11-08 15:54:09 +0000

    fix typo

---
+++ src/set_num_threads.jl
@@ -0,0 +1,16 @@
+const matmul_num_threads = Threads.nthreads()
+
+function set_num_threads(n::Int)
+
+    0 < n <=  Threads.nthreads() || throw(DomainError("The numbers of threads must be > 0 and <= $(Threads.nthreads())."))
+
+    global matmul_num_threads = n
+
+end
+
+
+function get_num_threads()
+
+    return matmul_num_threads
+    
+end
